a1fde85a23d0a551c9a46ec792743ea04eb95a48
morry48 <yyyyuuuuuuuu0408@gmail.com>
2022-09-05 00:36:04 +0900

    make blockchain mining

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module blockchain
 go 1.19
 
 require (
-	github.com/btcsuite/btcutil v1.0.2 // indirect
-	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"blockchain/block"
 	"blockchain/wallet"
 	"fmt"
 	"log"
@@ -11,8 +12,27 @@ func init() {
 }
 
 func main() {
-	w := wallet.NewWallet()
-	fmt.Println(w.PrivateKeyStr())
-	fmt.Println(w.PublicKeyStr())
-	fmt.Println(w.BlockchainAddress())
+	//w := wallet.NewWallet()
+	//fmt.Println(w.PrivateKeyStr())
+	//fmt.Println(w.PublicKeyStr())
+	//fmt.Println(w.BlockchainAddress())
+	//
+	//t := wallet.NewTransacation(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	//fmt.Printf("signature %s \n", t.GenerateSignature())
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
+
+	t := wallet.NewTransacation(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
+		walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added? ", isAdded)
+	blockchain.Mining()
+	blockchain.Print()
+
+	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletM.BlockchainAddress()))
 }
